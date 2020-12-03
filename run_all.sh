219f3d4ea06c56a0736a1202b24a02ab33b17ace
MaineK00n <dev.pylori1229@gmail.com>
2020-12-03 10:24:05 +0900

    update run_all.sh

--- run_all.sh
+++ run_all.sh
@@ -2,15 +2,31 @@
 
 TEST_FILES_PATH="/home/mainek00n/github/github.com/ehimetakahashilab/test_files"
 
+# parallel --results ${TEST_FILES_PATH}/no_dft/logs '/home/mainek00n/github/github.com/ehimetakahashilab/Logic-BIST-Sim/run_origin_circuit.sh /home/mainek00n/github/github.com/ehimetakahashilab/seq-benchmark/circuits/iscas89/{}' ::: s9234 s13207 s15850 s38417 s38584
+
+# for mode in "MulTPI" "Separate"
+# do
+#     for n_iter in 1
+#     do
+#         for cpi in "Hybrid-TPI" "Logic-CPI"
+#         do
+#             for topsis in "COP_Structure" "Structure"
+#             do
+#                 parallel --results ${TEST_FILES_PATH}/${mode}/${n_iter}iter/${cpi}/${topsis}/logs "/home/mainek00n/github/github.com/ehimetakahashilab/Logic-BIST-Sim/run_cp_op_circuit.sh /home/mainek00n/github/github.com/ehimetakahashilab/seq-benchmark/circuits/iscas89/{} ${TEST_FILES_PATH}/${mode}/${n_iter}iter/${cpi}/${topsis}/lab/fflist/{}/FF_STATION/TOPSIS ${TEST_FILES_PATH}/${mode}/${n_iter}iter/${cpi}/${topsis}/lab/cplist/{}" ::: s9234 s13207 s15850 s38417 s38584
+#             done
+#         done
+#     done
+# done
+
 for mode in "MulTPI" "MulTPI_reverse" "Separate"
 do
-    for n_iter in 1 5
+    for n_iter in 1 5 10
     do
         for cpi in "CPSC_top" "CPSF_top"
         do
             for topsis in "COP_Structure" "Structure"
             do
-                parallel --results ${TEST_FILES_PATH}/${mode}/${n_iter}iter/${cpi}/${topsis}/logs "./run_cp_op_circuit.sh /home/mainek00n/github/github.com/ehimetakahashilab/seq-benchmark/circuits/iscas89/{} ${TEST_FILES_PATH}/${mode}/${n_iter}iter/${cpi}/${topsis}/lab/fflist/{}/FF_STATION/TOPSIS ${TEST_FILES_PATH}/${mode}/${n_iter}iter/${cpi}/${topsis}/lab/cplist/{}" ::: s9234 s13207 s15850 s38417 s38584
+                parallel --results ${TEST_FILES_PATH}/${mode}/${n_iter}iter/${cpi}/${topsis}/logs "/home/mainek00n/github/github.com/ehimetakahashilab/Logic-BIST-Sim/run_cp_op_circuit.sh /home/mainek00n/github/github.com/ehimetakahashilab/seq-benchmark/circuits/iscas89/{} ${TEST_FILES_PATH}/${mode}/${n_iter}iter/${cpi}/${topsis}/lab/fflist/{}/FF_STATION/TOPSIS ${TEST_FILES_PATH}/${mode}/${n_iter}iter/${cpi}/${topsis}/lab/cplist/{}" ::: s9234 s13207 s15850 s38417 s38584
             done
         done
     done
