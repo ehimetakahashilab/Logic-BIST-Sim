d2baad09d5d82aa3f0c26a02969942936d1ab75e
MaineK00n <dev.pylori1229@gmail.com>
2020-12-29 05:03:08 +0900

    update run_all.sh

--- run_all.sh
+++ run_all.sh
@@ -18,17 +18,14 @@ TEST_FILES_PATH="/home/mainek00n/github/github.com/ehimetakahashilab/test_files"
 #     done
 # done
 
-for mode in "MulTPI" "MulTPI_reverse" "Separate"
+for mode in "MulTPI"
 do
     for n_iter in 1 5 10
     do
         for cpi in "CPSC_top" "CPSF_top"
         do
-            for topsis in "COP_Structure" "Structure"
-            do
-                parallel --results ${TEST_FILES_PATH}/${mode}/${n_iter}iter/${cpi}/${topsis}/logs "/home/mainek00n/github/github.com/ehimetakahashilab/Logic-BIST-Sim/run_cp_op_circuit.sh /home/mainek00n/github/github.com/ehimetakahashilab/seq-benchmark/circuits/iscas89/{} ${TEST_FILES_PATH}/${mode}/${n_iter}iter/${cpi}/${topsis}/lab/fflist/{}/FF_STATION/TOPSIS ${TEST_FILES_PATH}/${mode}/${n_iter}iter/${cpi}/${topsis}/lab/cplist/{}" ::: s9234 s13207 s15850 s38417 s38584
-                cp -r /home/mainek00n/github/github.com/ehimetakahashilab/Logic-BIST-Sim/OUTPUTS/FLT_DET_LIST ${TEST_FILES_PATH}/${mode}/${n_iter}iter/${cpi}/${topsis}/logs
-            done
+            parallel --results ${TEST_FILES_PATH}/${mode}/${n_iter}iter/${cpi}/logs "/home/mainek00n/github/github.com/ehimetakahashilab/Logic-BIST-Sim/run_cp_op_circuit.sh /home/mainek00n/github/github.com/ehimetakahashilab/seq-benchmark/circuits/iscas89/{} ${TEST_FILES_PATH}/${mode}/${n_iter}iter/${cpi}/${topsis}/lab/fflist/{}/FF_STATION/TOPSIS ${TEST_FILES_PATH}/${mode}/${n_iter}iter/${cpi}/${topsis}/lab/cplist/{}" ::: s9234 s13207 s15850 s38417 s38584
+            cp -r /home/mainek00n/github/github.com/ehimetakahashilab/Logic-BIST-Sim/OUTPUTS/FLT_DET_LIST ${TEST_FILES_PATH}/${mode}/${n_iter}iter/${cpi}/logs
         done
     done
 done
